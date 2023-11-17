a0f0b17e0625e788ddac5c96627a88aa94e84c80
Joao-Uchoas <70238880+Joao-Uchoas@users.noreply.github.com>
2023-11-17 09:02:04 -0300

    integracao

--- src/services/mealApi/mealsService.ts
+++ src/services/mealApi/mealsService.ts
@@ -3,32 +3,32 @@ import apiService from './apiService'
 import { Nullable } from '@/types'
 import { Meal } from '@/services/mealApi/models/Meal'
 
-export const getMeals = async (documentId: string): Promise<Meal | null> => {
+export const getMeals = async (userId: string): Promise<Meal | null> => {
   try {
-    const response = await apiService.get(`/meals/get?documentId=${documentId}`)
+    const response = await apiService.get(`/meals/get?userId=${userId}`)
     return response.data as Meal
   } catch (error) {
     console.error('Error getting meals:', error)
     return null
   }
 }
-export const createMeals = async (data: Nullable<Meal>, documentId: string): Promise<void> => {
+export const createMeals = async (data: Nullable<Meal>, userId: string): Promise<void> => {
   try {
-    await apiService.post(`/meals/create`, data)
+    await apiService.post(`/meals/create?userId=${userId}`, data)
   } catch (error) {
     console.error('Error creating meals:', error)
   }
 }
-export const updateMeals = async (documentId: string, data: Meal): Promise<void> => {
+export const updateMeals = async (userId: string, data: Meal): Promise<void> => {
   try {
-    await apiService.put(`/meals/update?documentId=${documentId}`, data)
+    await apiService.put(`/meals/update?userId=${userId}`, data)
   } catch (error) {
     console.error('Error updating meals:', error)
   }
 }
-export const deleteMeals = async (documentId: string): Promise<void> => {
+export const deleteMeals = async (userId: string): Promise<void> => {
   try {
-    await apiService.delete(`/meals/delete?documentId=${documentId}`)
+    await apiService.delete(`/meals/delete?userId=${userId}`)
   } catch (error) {
     console.error('Error deleting meals:', error)
   }
